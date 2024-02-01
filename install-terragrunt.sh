eb1243734e6e4c05a08f761a2f64945afb1256dd
Your Name <you@example.com>
2024-02-01 10:46:22 +0000

    install-terraform

---
+++ install-terragrunt.sh
@@ -0,0 +1,4 @@
+ wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.54.22/terragrunt_linux_amd64
+ mv terragrunt_linux_amd64 terragrunt
+ mv terragrunt /usr/local/bin/
+ chmod +x /usr/local/bin/terragrunt 
